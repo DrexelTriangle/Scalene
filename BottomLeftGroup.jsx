7d4a141e189ed2e626861f3df872232ea55f24f4
fweamy <baohanmi@gmail.com>
2025-06-23 13:53:33 +0700

    Added Bottom Groups

---
+++ BottomLeftGroup.jsx
@@ -0,0 +1,9 @@
+const BottomLeftGroup = ({ components }) => {
+    return (
+        <div className="col-span-8 divide-y divide-blue-700">
+            {components}
+        </div>
+    )
+}
+
+export default BottomLeftGroup
---
+++ BottomRightGroup.jsx
@@ -0,0 +1,9 @@
+const BottomRightGroup = ({ components }) => {
+    return (
+        <div className="col-span-4">
+            {components}
+        </div>
+    )
+}
+
+export default BottomRightGroup
