38b16f5314d938207e7cd115b945c9155762b1f7
fweamy <baohanmi@gmail.com>
2025-06-23 13:55:56 +0700

    Changed ArticleSection to FullWidth

---
+++ src/components/FullWidthGroup.jsx
@@ -0,0 +1,29 @@
+import List from './List.jsx';
+
+const FullWidthGroup = ({ sectionName, type }) => {
+    sectionName = sectionName.toUpperCase()
+        var articleGroup = (<>
+                <div className="col-span-5">
+                <List showImage={true} showDescription={true}  articleList={["0"]}/>
+                </div>
+                <div className="col-span-3">
+                <List showImage={true} articleList={["0", "1"]}/>
+                </div>
+                <div className="col-span-4">
+                <List showSection={true} articleList={["SPORTS", "OPINION", "NEWS", "COMICS"]}/>
+                </div>
+            </>
+        );
+        return (
+        <div>
+            <hr className='border-blue-700'/>
+            {sectionName}
+            <div className="grid grid-cols-12 gap-1 m-1 divide-x divide-grey">
+                {articleGroup}
+            </div>
+        </div>
+    )
+    }
+
+
+export default FullWidthGroup
