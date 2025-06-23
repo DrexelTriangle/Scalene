7b96237ba0d179fa4ff69a87f12749e9eb7fe86f
fweamy <baohanmi@gmail.com>
2025-06-23 16:38:41 +0700

    Added random placeholder articles

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,18 +4,17 @@ const List = ({showSection, showImage, showDescription, articleList, isRow}) => 
     articleList.map((articleName) =>
     {
         const sectionName = showSection ? `${articleName.toUpperCase()}` : ``
-        const image = showImage ? <img className="aspect-4/3 object-cover" src="src\assets\drexel-campus-bridge.jpg" alt="Image goes here"/> : <></>
-        const description = showDescription ? `This is a beautifully written description. It's non-existent, just like Drexel's financial management.` : ``
-        const title = randomListName()
+        const image = showImage ? <img className="aspect-4/3 object-cover" src={randomImage()} alt="Image goes here"/> : <></>
+        const description = showDescription ? `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.` : ``
 
         articleListComponent.push(
         <>
-        <div className="p-2">
+        <div className="p-3">
             {image}  
             <div className='secondary-color-two font-roboto-slab text-sm mt-1 font-semibold'>
                 {sectionName}
             </div>
-            <div className={`font-playfair ${articleList.length == 1 ? "text-3xl" : "text-xl"}`}> {title}
+            <div className={`font-playfair ${articleList.length == 1 ? "text-3xl" : "text-xl"}`}> {randomName()}
 </div>
             <div className="text-xs font-roboto-slab my-1">Author | Date</div>
             <p className="">{description}</p>
@@ -36,7 +35,7 @@ const List = ({showSection, showImage, showDescription, articleList, isRow}) => 
 
 export default List
 
-function randomListName() {
+function randomName() {
     var names = [
         "Raise your spirits at the new Fine Wine", 
         "Students weigh in on DA Krasner’s likely third term",
@@ -46,4 +45,14 @@ function randomListName() {
     ] 
     const randomName = names[Math.floor(Math.random() * names.length)]
     return randomName
+}
+
+function randomImage() {
+    var imgs = [
+        "src\\assets\\drexel-campus-bridge.jpg", 
+        "src\\assets\\jordan-fink.jpg",
+        "src\\assets\\KaseyShamis-2.jpeg",
+    ] 
+    const randomImg = imgs[Math.floor(Math.random() * imgs.length)]
+    return randomImg
 }
