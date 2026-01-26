74470b69a97d359aa5a77d411151733ca5dd42d7
mdepstein0 <mdepstein0@gmail.com>
2026-01-25 21:19:50 -0500

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node(),
   integrations: [react(), tailwind()]
 });
