b7904e24a8ebf40f46b879b0841e653a08b50bf5
mdepstein0 <mdepstein0@gmail.com>
2026-02-01 12:58:02 -0500

    update image proxy

--- src/pages/proxy/[...image].ts
+++ src/pages/proxy/[...image].ts
@@ -3,7 +3,7 @@ export const prerender = false;
 import type { APIRoute, GetStaticPaths } from 'astro';
 
 export async function GET({ params }) {
-  const url = "https://www.thetriangle.org/" + params.image;
+  const url = "https://cms.thetriangle.org/" + params.image;
   const res = await fetch(url);
 
   return new Response(await res.arrayBuffer(), {
