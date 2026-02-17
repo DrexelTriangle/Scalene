89642f63505312bf19d7826104da40ebeb72ff3e
Daniel Epstein <mdepstein0@gmail.com>
2026-02-16 23:36:36 -0500

    forgor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,33 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone', // required!
   }),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: ["favicon.svg", "robots.txt"],
+        manifest: {
+          name: "The Triangle",
+          short_name: "The Triangle",
+          start_url: "/",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#2563EB",
+          icons: [
+            {
+              src: "/logo.png",
+              sizes: "192x192",
+              type: "image/png"
+            },
+            {
+              src: "/logo.png",
+              sizes: "512x512",
+              type: "image/png"
+            }
+          ]
+        }
+      })
+    ]
+  }
 });
